86a1137bc1d11700e6e6e52fa3e7b680a587f764
Leo Riviera <leo.riviera@voxa.dev>
2022-09-14 22:16:47 +0100

    fix: broken migrations

--- migrations/20211122151130_create_user_id_idx.up.sql
+++ migrations/20211122151130_create_user_id_idx.up.sql
@@ -1,2 +1,2 @@
 -- create index on identities.user_id
-CREATE INDEX IF NOT EXISTS identities_user_id_idx ON {{ index.Options "Namespace" }}.identities using btree (user_id);
+CREATE INDEX IF NOT EXISTS identities_user_id_idx ON {{ index .Options "Namespace" }}.identities using btree (user_id);
